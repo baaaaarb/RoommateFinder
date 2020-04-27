0956fd055eccbed7c42a9b40baaeff645e9eff0c
baaaaarb <57261788+baaaaarb@users.noreply.github.com>
2020-04-27 17:25:57 +0200

    Update db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,30 +1,18 @@
-import mongo from 'mongodb'
+const {MongoClient, ObjectID} = require('mongodb') //brzi nacin - dekonstruktor
 
-let connection_string = 'mongodb+srv://admin:rfinder@cluster0-mbr1a.mongodb.net/test?retryWrites=true&w=majority'
+const connectionURL = 'mongodb+srv://admin:rfinder@cluster0-mbr1a.mongodb.net/test?retryWrites=true&w=majority'
 
-let client = new mongo.MongoClient(connection_string, {
-    useNewUrlParser: true,  //
-    useUnifiedTopology: true // bitna je kad je rad na vise?? neceg
+const databaseName= 'r-finder'
+
+
+
+
+MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {//poziva se kad je povezano
+    if (error) {
+        return console.log('Nemoguce povezati sa bazom')
+    }
+
+    const db = client.db(databaseName)
+
+    
 })
-
-let db=null
-
-export default ()=>{
-    return new Promise((resolve, reject) => {
-
-        if(db && client.isConnected()){ //provjerava je li baza postoji i je si spojen na klijent
-            resolve(db)
-        }
-
-        client.connect(err => {
-            if (err) {
-                reject('Doslo je do greske prilikom spajanja' + err)
-            }
-            else {
-                console.log('Uspjesno spajanje na bazu')
-                db = client.db('RoommateFinder')
-                resolve(db)
-            }
-        })
-    })
-}
